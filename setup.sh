cbb597723bc985b19cacd80f4c89eb54ae49bb1c
Siddharth Dhulipalla <dsiddharth@gmail.com>
2017-10-14 10:52:59 -0700

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install -y git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils libmodule-build-perl libavahi-compat-libdnssd-dev nodejs vim
+npm install
+npm start
